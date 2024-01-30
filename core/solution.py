Fixed sign errors

core/solution.py
@@ -75,8 +75,9 @@ def transform_to_zernike(hsh_vector):
 def zernike_FT(n,m):
     #HAVE TO MULTIPLY BY A(l,m) ALSO
     def bessel(rho,phi):
-        angular = np.where(m>=0,np.cos(np.abs(m)*phi), np.sin(np.abs(m)*phi))
-        res = (-1)**(n/2-abs(m)) * 2*np.pi*jv(n+1, rho) * angular / rho
+        angular = np.where(m>=0,np.cos(m*phi), np.sin(-m*phi))
+        #someone who is good at signs please help me simplify this. my family is dying
+        res = (-1)**(n/2-KroneckerDelta(m,0)+1+n) * 2*np.pi*jv(n+1, rho) * angular / rho
         return res
     return bessel
 #############################################
@@ -86,6 +87,7 @@ def CHSH_FT(l,m):
     """Returns a callable function of rho, phi that returns the FT of the (l,m)th element of the CHSH basis"""
     def sph_bessel(rho,phi):
         angular = np.where(m>=0,np.cos(np.abs(m)*phi), np.sin(np.abs(m)*phi))
-        res = A(l,np.abs(m))*2*np.pi*(1j)**np.abs(m) * (-1)**(l+1) * factorial2(l+np.abs(m), exact=True) / factorial2(l-np.abs(m)-1, exact=True) * spherical_jn(l,rho) * angular / rho
+        #someone who is good at signs please help me simplify this. my family is dying
+        res = A(l,np.abs(m))*2*np.pi*(1j)**(abs(m)) * (-1)**(2*l-1-KroneckerDelta(m,0)) * factorial2(l+np.abs(m), exact=True) / factorial2(l-np.abs(m)-1, exact=True) * spherical_jn(l,rho) * angular / rho
         return  res
     return sph_bessel

core/visibility.py
@@ -15,19 +15,32 @@ config.update("jax_enable_x64", True)
 
 
 jax_funcs = get_ylm_FTs()
+
+def left_project(deg, M, theta, inc, obl):
+    # Note that here we are using the fact that R . M = (M^T . R^T)^T
+    MT = jnp.transpose(M)
+
+    # Rotate to the polar frame
+    MT = dot_rotation_matrix(deg, 1.0, 0.0, 0.0, -0.5 * jnp.pi)(MT)
+    MT = dot_rotation_matrix(deg, None, None, 1.0, -theta)(MT)
+
+    # Rotate to the sky frame
+    MT = dot_rotation_matrix(deg, 1.0, 0.0, 0.0, 0.5 * jnp.pi)(MT)
+    MT = dot_rotation_matrix(deg, None, None, 1.0, -obl)(MT)
+    MT = dot_rotation_matrix(
+        deg, -jnp.cos(obl), -jnp.sin(obl), 0.0, 0.5 * jnp.pi - inc
+    )(MT)
+
+    return MT
+
 def analytic_v(l_max,u,v,inc,obl,theta,y):
     rho = jnp.sqrt(u**2 + v**2)
     phi = jnp.arctan2(v,u)
     
     #start with y, do Rdot (not dotR) for each step in v except in regular order (read from top down in order)
     #end with rotated y
-    #apply solution matrix at left to rotated y inside a for loop or the like
-    x = Rdot(l_max, jnp.array([1.0, 0.0, 0.0]))(y,0.5 * jnp.pi)
-    x = Rdot(l_max, jnp.array([0, 0, 1.0]))(x,theta)
-    x = Rdot(l_max, jnp.array([1.0, 0.0, 0.0]))(x,-0.5 * jnp.pi)
-    x = Rdot(l_max, jnp.array([0, 0, 1.0]))(x,obl)
-    x = Rdot(
-    l_max, jnp.array([-jnp.cos(obl), -jnp.sin(obl), 0.0]))(x,-(0.5 * jnp.pi - inc))
+    
+    Ry = left_project(l_max, y, theta, inc, obl)
     
     ft = np.zeros_like(rho)
     y_hsh = []
@@ -35,18 +48,47 @@ def analytic_v(l_max,u,v,inc,obl,theta,y):
         for m in range(-l,l+1):
             #HSH
             if (l+m)%2==0:
-                y_hsh.append(x[l**2+l+m])
+                y_hsh.append(Ry[l**2+l+m])
             else:
-                ft += CHSH_FT(l,m)(rho,phi)*x[l**2+l+m]
+                ft += CHSH_FT(l,m)(rho,phi)*Ry[l**2+l+m]
     y_hsh = np.array(y_hsh)
-    print(len(y_hsh))
+    #print(theta, x)
     zs = transform_to_zernike(y_hsh)
     for j in range(len(zs)):
         n,m = j_to_nm(j)
         ft += zs[j]*zernike_FT(n,m)(rho,phi)
     return ft
         
+def v_alt(l_max, u,v,inc,obl,theta, y):
+    """
+    Computes the visibility function for a given spherical harmonic map
+    represented using the spherical harmonic coefficient vector y.
+    Rotates the star to an orientation on sky given by inclination (inc)
+    obliquity (obl), and theta (phase). The visibility function returns 
+
+    Args:
+        u (float): _description_
+        v (float): _description_
+        inc (float): _description_
+        obl (float): _description_
+        theta (float): _description_
+        y (array): _description_
+    """
+    rho = jnp.sqrt(u**2 + v**2)
+    phi = jnp.arctan2(v,u)
+    nmax = l_max**2 + 2 * l_max + 1
+    fT = []
+    for l in range(l_max+1):
+        for m in range(-l,l+1):
+            fT.append(eval(jax_funcs[(l,m)]))
+    fT = jnp.array(fT)
     
+    Ry = left_project(l_max, y, theta, inc, obl)
+    res = np.zeros_like(rho)
+    for i, v in enumerate(Ry):
+        res+=v*fT[i]
+    return res
+        
     
 def v(l_max, u,v,inc,obl,theta, y):
     """