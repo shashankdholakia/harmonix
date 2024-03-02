5794eb64d585b21ec51649fa686b02e02c2b3b74
Shashank Dholakia <shashankdholakia1@gmail.com>
2024-03-03 00:03:37 +1000

    Updated to new version of jaxoplanet

--- src/harmonix/harmonix.py
+++ src/harmonix/harmonix.py
@@ -1,5 +1,6 @@
 import jax
-from jaxoplanet.experimental.starry.wigner import dot_rotation_matrix
+from jaxoplanet.experimental.starry.rotation import dot_rotation_matrix
+from jaxoplanet.experimental.starry.basis import A1
 import jax.numpy as jnp
 from jax import config
 from jax import grad, vmap, jit
@@ -53,7 +54,7 @@ class Harmonix(Base):
         y_hsh = Ry[self.hsh_inds]
         y_chsh = Ry[self.chsh_inds]
         zernike_coeffs = transform_to_zernike(y_hsh)
-        return ft_hsh@zernike_coeffs + ft_chsh@y_chsh
+        return (ft_hsh@zernike_coeffs + ft_chsh@y_chsh)/(rTA1(self.l_max)@Ry)
     
 
 @partial(jit, static_argnums=0)
@@ -75,3 +76,44 @@ def left_project(deg, M, theta, inc, obl):
     return MT
     
     
+def rT(lmax):
+    rt = [0 for _ in range((lmax + 1) * (lmax + 1))]
+    amp0 = jnp.pi
+    lfac1 = 1.0
+    lfac2 = 2.0 / 3.0
+    for ell in range(0, lmax + 1, 4):
+        amp = amp0
+        for m in range(0, ell + 1, 4):
+            mu = ell - m
+            nu = ell + m
+            rt[ell * ell + ell + m] = amp * lfac1
+            rt[ell * ell + ell - m] = amp * lfac1
+            if ell < lmax:
+                rt[(ell + 1) * (ell + 1) + ell + m + 1] = amp * lfac2
+                rt[(ell + 1) * (ell + 1) + ell - m + 1] = amp * lfac2
+            amp *= (nu + 2.0) / (mu - 2.0)
+        lfac1 /= (ell / 2 + 2) * (ell / 2 + 3)
+        lfac2 /= (ell / 2 + 2.5) * (ell / 2 + 3.5)
+        amp0 *= 0.0625 * (ell + 2) * (ell + 2)
+
+    amp0 = 0.5 * jnp.pi
+    lfac1 = 0.5
+    lfac2 = 4.0 / 15.0
+    for ell in range(2, lmax + 1, 4):
+        amp = amp0
+        for m in range(2, ell + 1, 4):
+            mu = ell - m
+            nu = ell + m
+            rt[ell * ell + ell + m] = amp * lfac1
+            rt[ell * ell + ell - m] = amp * lfac1
+            if ell < lmax:
+                rt[(ell + 1) * (ell + 1) + ell + m + 1] = amp * lfac2
+                rt[(ell + 1) * (ell + 1) + ell - m + 1] = amp * lfac2
+            amp *= (nu + 2.0) / (mu - 2.0)
+        lfac1 /= (ell / 2 + 2) * (ell / 2 + 3)
+        lfac2 /= (ell / 2 + 2.5) * (ell / 2 + 3.5)
+        amp0 *= 0.0625 * ell * (ell + 4)
+    return np.array(rt)
+
+def rTA1(lmax):
+    return rT(lmax) @ A1(lmax)
