Limb darkening filter implemented, l=0, l=1 edge cases fixed and minus sign issues solved

src/harmonix/harmonix.py
@@ -23,6 +23,8 @@ class Harmonix(Base):
     hsh_inds: jnp.ndarray
     chsh_inds: jnp.ndarray
     data: jnp.ndarray
+    u: jnp.ndarray
+    pu: jnp.ndarray
 
     def __init__(self, surface):
         """Class for computation of interferometric observables from a spherical harmonic map
@@ -36,9 +38,27 @@ class Harmonix(Base):
         """
         self.surface = surface
         self.data = surface.y.todense()
-        l_max = surface.y.deg
+        self.u = jnp.array(surface.u)
+        l_max = surface.deg
         n_max = l_max**2 + 2 * l_max + 1
         hsh_mask = np.zeros(n_max, dtype=bool)
+        
+        #We can precompute the limb darkening filter here
+        #because it doesn't change with time
+        udeg = surface.udeg
+        # limb darkening
+        if surface.udeg == 0:
+            pu = Pijk.from_dense(jnp.array([1]))
+        else:
+            u = jnp.array([1, *surface.u])
+            pu = Pijk.from_dense(u @ U(surface.udeg), degree=surface.udeg)
+        self.pu = pu
+        
+        # yu = jnp.array(np.linalg.inv(A1(udeg).todense())) @ pu
+        # yu = Ylm.from_dense(yu.flatten(), normalize=False)
+        # norm = 1 / (Pijk.from_dense(pu, degree=udeg).todense() @ solution.rT(udeg))
+        # yu = yu * norm
+        # self.yu = yu
         
         for l in range(l_max+1):
             for m in range(-l,l+1):
@@ -54,28 +74,28 @@ class Harmonix(Base):
         else:
             return 2 * jnp.pi * time / self.surface.period
         
-    #@jit
     def model(self, u, v, t):
         rho = jnp.sqrt(u**2 + v**2)
         phi = jnp.arctan2(v,u)
-        ft_hsh, ft_chsh = solution_vector(self.surface.ydeg)(rho, phi)
+        ft_hsh, ft_chsh = solution_vector(self.surface.deg)(rho, phi)
         theta = self.rotational_phase(t)
-        Ry = left_project(jnp.array(self.surface.ydeg), self.surface.inc, self.surface.obl, theta, 0.0, self.data)
-        #add limb darkening
-        """
-        udeg = self.surface.udeg
-        _u = jnp.array([1, *self.surface.u])
-        pu = _u @ U(udeg)
-        yu = jnp.array(np.linalg.inv(A1(udeg).todense()) @ pu)
-        yu = Ylm.from_dense(yu.flatten(), normalize=False)
-        norm = 1 / (Pijk.from_dense(pu, degree=udeg).todense() @ solution.rT(udeg))
-        yu = yu * norm
-        Ry = (Ry*yu).todense()
-        """
-        y_hsh = Ry[self.hsh_inds]
-        y_chsh = Ry[self.chsh_inds]
+        #start by rotating the map (before adding the limb darkening filter)
+        Ry = left_project(self.surface.ydeg, self.surface.inc, self.surface.obl, theta, 0.0, self.data)
+        
+        #convert the map to the polynomial basis
+        p_y = Pijk.from_dense(A1(self.surface.ydeg) @ Ry, degree=self.surface.ydeg)
+        #multiply the map by the limb darkening map in polynomial basis
+        p_yu = p_y * self.pu
+        p_yu = p_yu.todense().flatten()
+        #don't know why I need this normalization term but I got it from jaxoplanet.starry
+        norm = np.pi / (self.pu.tosparse() @ rT(self.surface.udeg))
+        #convert back into spherical harmonic basis
+        Ry_mul = self.surface.amplitude * (np.array(np.linalg.inv(A1(self.surface.deg).todense()) @ p_yu)) * norm
+        
+        y_hsh = Ry_mul[self.hsh_inds]
+        y_chsh = Ry_mul[self.chsh_inds]
         zernike_coeffs = transform_to_zernike(y_hsh)
-        return (ft_hsh@zernike_coeffs + ft_chsh@y_chsh)/(rTA1(self.surface.y.deg)@Ry)/np.sqrt(np.pi)*2
+        return (ft_hsh@zernike_coeffs + ft_chsh@y_chsh)/(rTA1(self.surface.deg)@Ry_mul)/np.sqrt(np.pi)*2
     
 
     

src/harmonix/solution.py
@@ -83,7 +83,7 @@ def zernike_FT(n,m):
     def bessel(rho,phi):
         angular = np.where(m>=0,np.cos(m*phi), np.sin(-m*phi))
         #someone who is good at signs please help me simplify this. my family is dying
-        res = (-1)**(n/2-KroneckerDelta(m,0)+1+n) * 2*np.pi*jv(n+1, rho) * angular / rho
+        res = 1j**(n)*(-1)**(n+m) * 2*np.pi*jv(n+1, rho) * angular / rho
         return res
     return bessel
 
@@ -92,7 +92,7 @@ def zernike_FT_jax(n,m, Jvs):
     def bessel(rho,phi):
         angular = jnp.where(m>=0,jnp.cos(m*phi), jnp.sin(-m*phi))
         #someone who is good at signs please help me simplify this. my family is dying
-        res = (-1)**(n/2-KroneckerDelta(m,0)+1+n) * 2*jnp.pi*Jvs[n+1] * angular / rho
+        res = 1j**(n)*(-1)**(n+m) * 2*jnp.pi*Jvs[n+1] * angular / rho
         return res
     return bessel
 
@@ -104,7 +104,7 @@ def CHSH_FT(l,m):
     def sph_bessel(rho,phi):
         angular = np.where(m>=0,np.cos(m*phi), np.sin(np.abs(m)*phi))
         #someone who is good at signs please help me simplify this. my family is dying
-        res = A(l,np.abs(m))*2*np.pi*(1j)**(abs(m)) * (-1)**(2*l-1-KroneckerDelta(m,0)) * factorial2(l+np.abs(m), exact=True) / factorial2(l-np.abs(m)-1, exact=True) * spherical_jn(l,rho) * angular / rho
+        res = A(l,np.abs(m))*2*np.pi*(1j)**(abs(m)) * (-1)**(m) * factorial2(l+np.abs(m), exact=True) / factorial2(l-np.abs(m)-1, exact=True) * spherical_jn(l,rho) * angular / rho
         return  res
     return sph_bessel
 
@@ -113,7 +113,7 @@ def CHSH_FT_jax(l,m, jvs):
     def sph_bessel(rho,phi):
         angular = jnp.where(m>=0,jnp.cos(m*phi), jnp.sin(jnp.abs(m)*phi))
         #someone who is good at signs please help me simplify this. my family is dying
-        res = A(l,np.abs(m))*2*jnp.pi*(1j)**(abs(m)) * (-1)**(2*l-1-KroneckerDelta(m,0)) * factorial2(l+np.abs(m), exact=True) / factorial2(l-np.abs(m)-1, exact=True) * jvs[l] * angular / rho
+        res = A(l,np.abs(m))*2*jnp.pi*(1j)**(abs(m)) * (-1)**(m) * factorial2(l+np.abs(m), exact=True) / factorial2(l-np.abs(m)-1, exact=True) * jvs[l] * angular / rho
         return  res
     return sph_bessel
 
@@ -121,8 +121,9 @@ def solution_vector(l_max):
     """Returns a function that constructs the two part Fourier solution vector given rho, phi"""
     n_max = l_max**2 + 2 * l_max + 1
     j_max = jmax(l_max)
+    
     @jax.jit
-    @partial(jnp.vectorize, signature=f"(),()->({j_max}),({n_max-j_max})")
+    @partial(jnp.vectorize, signature=f"(),()->({j_max+1}),({n_max-j_max-1})")
     def impl(rho, phi):
         ft_hsh = []
         ft_chsh = []
@@ -135,5 +136,5 @@ def solution_vector(l_max):
                     ft_hsh.append(zernike_FT_jax(l,m,bessels)(rho,phi))
                 else:
                     ft_chsh.append(CHSH_FT_jax(l,m, spherical_bessels)(rho,phi))
-        return jnp.stack(ft_hsh), jnp.stack(ft_chsh)
+        return jnp.atleast_1d(ft_hsh), jnp.atleast_1d(ft_chsh)
     return impl

src/harmonix/test_utils.py
@@ -46,7 +46,7 @@ def v_scipy(l_max,u,v,inc,obl,theta,y):
     #start with y, do Rdot (not dotR) for each step in v except in regular order (read from top down in order)
     #end with rotated y
     
-    Ry = left_project(l_max, y, theta, inc, obl)
+    Ry = left_project(l_max, inc, obl, theta, 0.0, y)
     
     ft = np.zeros_like(rho)
     y_hsh = []